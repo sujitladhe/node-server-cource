6aba75ac4b6ecfa2b46149e70c9238ac1996da26
sujitladhe <sujit.ladhe88@gmail.com>
2018-08-14 17:44:03 +0530

    project page added

--- server.js
+++ server.js
@@ -48,6 +48,12 @@ app.get('/about', (req, res) => {
 	});
 });
 
+app.get('/projects', (req, res) => {
+	res.render('projects.hbs', {
+		pageTitle: 'projects page here.'
+	});
+});
+
 app.get('/bad', (req, res) => {
 	res.send({errorMessage: 'BAD Request'});
 });
